a2cc11d00f45f60df694bbd703ce7725d1e56fa1
SWCL0 <stan.w.c@icloud.com>
2024-02-08 10:58:33 +0100

    Update hoofdletters.php
    andere 2 functies toegevoegd

--- hoofdletters.php
+++ hoofdletters.php
@@ -5,8 +5,12 @@ $zin = readline();
 
 $caps = strtoupper($zin);
 $lowercase = strtolower($zin);
+$allFirstCaps = ucwords($zin);
+$firstCaps = ucfirst($zin);
 
 echo $caps . PHP_EOL;
 echo $lowercase . PHP_EOL;
+echo $allFirstCaps . PHP_EOL;
+echo $firstCaps . PHP_EOL;
 
 ?>
